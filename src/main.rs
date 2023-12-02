ad6b99f764a5b2820c375c29f9696db9f0a4134c
Cameron VanHouzen <camvh@live.com>
2023-12-02 07:24:13 +0000

    day 1 bonus

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn hello_error() -> StatusCode {
+    StatusCode::INTERNAL_SERVER_ERROR
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(hello_error));
 
     Ok(router.into())
 }
