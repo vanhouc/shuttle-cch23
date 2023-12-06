268401967dd3b858a47ea68b9fdae74dde314b44
Cameron VanHouzen <camvh@live.com>
2023-12-06 18:30:27 +0000

    day 1 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +8,16 @@ async fn hello_error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
+async fn day1(Path((num1, num2)): Path<(u64, u64)>) -> String {
+    (num1 ^ num2).pow(3).to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(hello_error));
+        .route("/-1/error", get(hello_error))
+        .route("/1/:num1/:num2", get(day1));
 
     Ok(router.into())
 }
