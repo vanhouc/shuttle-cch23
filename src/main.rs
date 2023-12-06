e34c242056e868dbb3cb9d0afb0dc3903b5a0a28
Cameron VanHouzen <camvh@live.com>
2023-12-06 18:56:53 +0000

    day 1 task 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::{ErrorResponse, Result},
+    routing::get,
+    Router,
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,8 +14,21 @@ async fn hello_error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
-async fn day1(Path((num1, num2)): Path<(u64, u64)>) -> String {
-    (num1 ^ num2).pow(3).to_string()
+async fn day1(Path(ids): Path<String>) -> Result<String> {
+    let ids: Vec<u32> = ids
+        .split('/')
+        .map(|id| {
+            id.parse()
+                .map_err(|_| ErrorResponse::from(StatusCode::BAD_REQUEST))
+        })
+        .collect::<Result<Vec<_>>>()?;
+    let response = ids
+        .into_iter()
+        .reduce(|acc, id| acc ^ id)
+        .ok_or(StatusCode::BAD_REQUEST)?
+        .pow(3)
+        .to_string();
+    Ok(response)
 }
 
 #[shuttle_runtime::main]
@@ -17,7 +36,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(hello_error))
-        .route("/1/:num1/:num2", get(day1));
+        .route("/1/*id", get(day1));
 
     Ok(router.into())
 }
