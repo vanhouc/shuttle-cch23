26d6664428b4b45d1abfd5deafa0bfc9e70af010
Cameron VanHouzen <camvh@live.com>
2023-12-08 14:59:15 +0000

    day 6 task 2

--- src/main.rs
+++ src/main.rs
@@ -112,11 +112,20 @@ async fn contest(Json(reindeer): Json<Vec<Reindeer>>) -> Result<Json<Competition
 #[derive(Serialize)]
 struct ElfCount {
     elf: usize,
+    #[serde(rename = "elf on a shelf")]
+    elf_shelves: usize,
+    #[serde(rename = "shelf with no elf on it")]
+    shelves: usize,
 }
 
-async fn elf(elf: String) -> Json<ElfCount> {
+async fn elf(body: String) -> Json<ElfCount> {
+    let elf = body.matches("elf").count();
+    let elf_shelves = body.matches("elf on a shelf").count();
+    let shelves = body.matches("shelf").count() - elf_shelves;
     Json(ElfCount {
-        elf: elf.matches("elf").count(),
+        elf,
+        elf_shelves,
+        shelves,
     })
 }
 
